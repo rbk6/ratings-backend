fa2a28bd9141877c10719ff79907cbcf5cc5ac47
rbk6 <rmbackman@gmail.com>
2023-08-20 13:30:15 -0700

    implemented password hashing and initial user login in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,58 @@
 // imports
 const db = require('../db')
+const bcrypt = require('bcryptjs')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
 
-/* todo: JWT integration, refresh/auth tokens, hash password, login */
+/* todo: JWT integration, refresh/auth tokens */
 
 const register = async (req, res) => {
   const { username, email, password, name } = req.body
   if (!username || !email || !password || !name)
     throw new BadRequestError('Must provide all fields to register')
-  const query = {
-    text: 'INSERT INTO "user"(username, email, password, name) VALUES($1, $2, $3, $4)',
-    values: [username, email, password, name],
-  }
   try {
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const query = {
+      text: 'INSERT INTO "user"(username, email, password, name) VALUES($1, $2, $3, $4)',
+      values: [username, email, hashedPassword, name],
+    }
     await db.query(query)
     return res.status(StatusCodes.CREATED).json({ status: 'success' })
   } catch (err) {
-    throw new BadRequestError(err.detail)
+    const msg = err.constraint.includes('username')
+      ? 'Username is taken, please provide a different one.'
+      : err.constraint.includes('email')
+      ? 'Email is already in use, please provide a different one.'
+      : 'Unable to create account, please try again.'
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: msg, status: 'failed' })
   }
 }
 
-const login = async (req, res) => {}
+const login = async (req, res) => {
+  // check if user exists
+  const { username, password } = req.body
+  const query = {
+    text: 'SELECT * FROM "user" WHERE username = $1 LIMIT 1',
+    values: [username],
+  }
+  try {
+    const user = await db.query(query)
+    if (!user || user.rowCount === 0)
+      throw new BadRequestError('Invalid username/password, please try again.')
+    if (await bcrypt.compare(password, user.rows[0].password)) {
+      res.json({ msg: 'Logged in successfully!' })
+    } else {
+      res.json({ msg: 'Invalid username/password, please try again.' })
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ msg: 'There was an error logging in, please try again.' })
+  }
+}
 
 module.exports = {
   register,
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const { register } = require('../controllers/auth')
+const { register, login } = require('../controllers/auth')
 
-router.route('/').post(register)
+router.route('/').post(register).get(login)
 
 module.exports = router
