3950922491c5d3cbbd1f4a11cc82490f7e5ba508
rbk6 <rmbackman@gmail.com>
2023-10-05 03:20:41 -0700

    updated movie/show object mapping and count per request for consistency

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,22 +5,45 @@ const { StatusCodes } = require('http-status-codes')
 
 const getMovies = async (req, res) => {
   const { page } = req.params
-  const options = {
-    method: 'GET',
-    url: process.env.TMDB_PAGE_URI + `?page=${page}`,
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer ' + process.env.TMDB_SECRET,
-    },
-  }
+  const pages = [
+    page,
+    parseInt(page) + 1,
+    parseInt(page) + 2,
+    parseInt(page) + 3,
+  ]
+  const requests = pages.map((page) => {
+    const options = {
+      method: 'GET',
+      url: process.env.TMDB_PAGE_URI + `?page=${page}`,
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer ' + process.env.TMDB_SECRET,
+      },
+    }
+    return axios.request(options)
+  })
   try {
-    const response = await axios.request(options)
-    const { data: movies } = response
+    const responses = await Promise.all(requests)
+    const results = responses.reduce(
+      (acc, response) => [...acc, ...response.data.results],
+      []
+    )
+
+    const formattedResults = results.map(
+      ({ id, original_title, overview, release_date, poster_path }) => ({
+        id: id,
+        title: original_title,
+        description: overview,
+        releaseDate: release_date,
+        image: `https://image.tmdb.org/t/p/w500/${poster_path}`,
+      })
+    )
 
     return res
       .status(StatusCodes.OK)
-      .json({ movieCount: movies.results.length, data: movies })
+      .json({ movieCount: results.length, data: formattedResults })
   } catch (err) {
+    console.log(err)
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       msg: 'An error occurred while getting movies, please try again.',
     })
--- controllers/shows.js
+++ controllers/shows.js
@@ -7,15 +7,33 @@ const { TooManyRequestsError } = require('../errors')
 // get pagination of shows
 const getShows = async (req, res) => {
   const { page } = req.params
+  const itemsPerPage = Math.ceil(250 / 3)
+  const apiPage = Math.floor(page / 3)
+  const startIndex = (page % 3) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+
   try {
     const response = await axios.get(
-      process.env.TVMAZE_URI + `/shows?page=${page}`
+      process.env.TVMAZE_URI + `/shows?page=${apiPage}`
     )
-    const { data: shows } = response
+
+    const shows = response.data.slice(startIndex, endIndex)
+
+    const formattedResults = shows.map(
+      ({ id, name, premiered, ended, status, image, summary }) => ({
+        id: id,
+        title: name,
+        releaseDate: premiered,
+        endDate: ended,
+        status: status,
+        image: image.original,
+        description: summary,
+      })
+    )
 
     return res
       .status(StatusCodes.OK)
-      .json({ showCount: shows.length, data: shows })
+      .json({ showCount: formattedResults.length, data: formattedResults })
   } catch (err) {
     if (err.response && err.response.status === StatusCodes.TOO_MANY_REQUESTS) {
       throw new TooManyRequestsError(
